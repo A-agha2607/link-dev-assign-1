df0cfdd90d371d527d1fd0606634ca403ab573ac
A-agha2607 <154236961+A-agha2607@users.noreply.github.com>
2025-08-13 03:04:58 +0300

    styled router project

--- REACT-ASSIGN/router-app/src/pages/Home.jsx
+++ REACT-ASSIGN/router-app/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 function Home() {
   return (
     <div className='d-flex flex-column align-items-center'>
-        <h2>Welcome to the home page!</h2>
+        <h2 className='text-light'>Welcome to the home page!</h2>
         <Link to={'/movies'}>click here to see movies page</Link>
 
     </div>
--- REACT-ASSIGN/router-app/src/pages/Movies.jsx
+++ REACT-ASSIGN/router-app/src/pages/Movies.jsx
@@ -7,11 +7,11 @@ import Card from 'react-bootstrap/Card';
 function Movies() {
     return (
         <div>
-            <h1 className='text-center'>Movies</h1>
+            <h1 className='text-center text-light'>Movies</h1>
             <div className='d-flex flex-column align-items-center'>
                 {movies.map((mov) => (
-                    <div className='pb-3'>
-                        <Card  style={{ width: '18rem' }} key={mov.id}>
+                    <div className='pb-3 crds-div'>
+                        <Card  style={{ width: '18rem' }} className='crds' key={mov.id}>
                             <Card.Body>
                                 <Card.Title>{mov.title}</Card.Title>
                                 <Card.Subtitle className="mb-2 text-muted">{mov.year}</Card.Subtitle>
